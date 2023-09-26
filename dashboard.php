d873921d4cb315cf826516e4bb5703e2b5c34368
Skypros98 <muhammadagusdwifasophian_2226250085@mhs.mdp.ac.id>
2023-09-26 15:05:14 +0700

    Update dashboard & Push logout

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,7 @@
 session_start();
 if ($_SESSION['npm'] == 2226250001) {
     echo "Selamat datang, " . $_SESSION['nama'];
+    echo "<br> <a href='logout.php>Logout</a>";
 } else {
     echo "Anda tidak diizinkan untuk mengakses halaman ini";
 }
