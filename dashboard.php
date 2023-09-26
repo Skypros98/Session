db292bd1c9824fff1f3665333e241022b2f71214
Skypros98 <muhammadagusdwifasophian_2226250085@mhs.mdp.ac.id>
2023-09-26 15:11:26 +0700

    Update Dashboard Php

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,10 @@
 <?php
-// halaman ini bisa diakses jika session npm = 2226250062
+// halaman ini bisa diakses jika session npm = 2226250001
 session_start();
-if ($_SESSION['npm'] == 2226250001) {
+if (isset($_SESSION['npm']) and $_SESSION['npm'] == 2226250001) {
     echo "Selamat datang, " . $_SESSION['nama'];
     echo "<br> <a href='logout.php>Logout</a>";
 } else {
     echo "Anda tidak diizinkan untuk mengakses halaman ini";
+    header("Location: session.php");
 }
